4130074437107ce01e72a365a8feebcf845dc5ef
tgaytan <tgaytan14@utexas.edu>
2023-10-04 21:50:15 -0500

    added the badge based off user input

--- newREADME.md
+++ newREADME.md
@@ -1,12 +1,11 @@
-[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)
 
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="86" height="20" role="img" aria-label="build: passed"><title>build: passed</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="r"><rect width="86" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="37" height="20" fill="#555"/><rect x="37" width="49" height="20" fill="#97ca00"/><rect width="86" height="20" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="195" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="270">build</text><text x="195" y="140" transform="scale(.1)" fill="#fff" textLength="270">build</text><text aria-hidden="true" x="605" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="390">passed</text><text x="605" y="140" transform="scale(.1)" fill="#fff" textLength="390">passed</text></g></svg>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="168" height="20" role="img" aria-label="License: Apache License 2.0"><title>License: Apache License 2.0</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="r"><rect width="168" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="51" height="20" fill="#555"/><rect x="51" width="117" height="20" fill="#97ca00"/><rect width="168" height="20" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="265" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="410">License</text><text x="265" y="140" transform="scale(.1)" fill="#fff" textLength="410">License</text><text aria-hidden="true" x="1085" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="1070">Apache License 2.0</text><text x="1085" y="140" transform="scale(.1)" fill="#fff" textLength="1070">Apache License 2.0</text></g></svg>
 
 # title
 
 ## Description 
 
-short desc
+desc
 
 ## Installation
 
@@ -14,12 +13,12 @@ none
 
 ## Usage
 
-none
+usage
 
 ## Contributing
 
-no
+none
 
 ## Tests
 
-can't
+run it
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,55 +1,70 @@
 const { makeBadge, ValidationError } = require('badge-maker')
 
-
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-// function renderLicenseBadge(license) {}
 var renderLicenseBadge = license => {
   const format = {
-    label: 'build',
-    message: 'passed',
+    label: 'License',
+    message: license,
     color: 'green'
   };
 
   const svg = makeBadge(format);
-  console.log(svg);
-  // try {
-  //   makeBadge({})
-  // } catch (e) {
-  //   console.log(e)
-  // }
+  
+  return svg ? svg : '';
+};
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+var renderLicenseLink = license => {
   return '';
 };
 
-renderLicenseBadge('license');
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+// function renderLicenseSection(license) {}
+var renderLicenseSection = license => {
+  return '';
+};
 
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// // function renderLicenseLink(license) {}
-// var renderLicenseLink = license => {
-//   return '';
-// };
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  const license = data.license;
+  const badge = renderLicenseBadge(license);
+  renderLicenseLink(license);
+  renderLicenseSection(license);
 
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// // function renderLicenseSection(license) {}
-// var renderLicenseSection = license => {
-//   return '';
-// };
+  return `
+${badge}
 
-// // TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-//   const license = data.license;
-//   renderLicenseBadge(license);
-//   renderLicenseLink(license);
-//   renderLicenseSection(license);
+# ${data.title}
 
-//   return `
-// # ${data.title}
+## Description 
 
-// ## Description 
+${data.description}
 
-// ${data.description}
+## Installation
+
+${data.installSteps}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.credits}
+
+## Tests
+
+${data.test}
+`;
+}
+
+module.exports = generateMarkdown;
+
+
+
+// ## Table of Contents
 
 // ## Installation
 
@@ -60,29 +75,5 @@ renderLicenseBadge('license');
 // ${data.usage}
 
 // ## Contributing
-
-// ${data.credits}
-
+// ## License ${data.license}
 // ## Tests
-
-// ${data.test}
-// `;
-// }
-
-// module.exports = generateMarkdown;
-
-
-
-// // ## Table of Contents
-
-// // ## Installation
-
-// // ${data.installSteps}
-
-// // ## Usage
-
-// // ${data.usage}
-
-// // ## Contributing
-// // ## License ${data.license}
-// // ## Tests
